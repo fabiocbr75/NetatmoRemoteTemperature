b3d82a1714838ecec63eedd4d50f6ac9ee6b9d54
Fabio Panero <fabio.pnr@gmail.com>
2018-12-11 21:19:09 +0100

    First checkin

---
+++ TemperatureHub/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TemperatureHub.Controllers
+{
+    public class RoomTemp
+    {
+        public string MAC { get; set; }
+        public float Temp { get; set; }
+        public float Humidity { get; set; }
+    }
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { DateTime.Now.ToLongTimeString() };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] RoomTemp value)
+        {
+            Console.WriteLine($"Time:{DateTime.Now.ToString("s")} MAC:{value.MAC}; Temp:{value.Temp}; Humidity:{value.Humidity}");
+        }
+    }
+}
