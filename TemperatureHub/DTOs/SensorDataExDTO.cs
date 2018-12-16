9a9b23da06dd9380efcd767de90c84cedced6f53
Fabio Panero <fabio.pnr@gmail.com>
2018-12-16 13:33:40 +0100

    Added HeatIndex

--- TemperatureHub/DTOs/SensorDataExDTO.cs
+++ TemperatureHub/DTOs/SensorDataExDTO.cs
@@ -5,6 +5,7 @@
         public string MAC { get; set; }
         public string Name { get; set; }
         public double Temp { get; set; }
+        public double HeatIndex { get; set; }
         public double Humidity { get; set; }
         public string IngestionTimestamp { get; set; }
     }
