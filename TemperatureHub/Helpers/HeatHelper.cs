2e523e961973a97ca0e5d32843a1dd468008ce43
Fabio Panero <fabio.pnr@gmail.com>
2018-12-16 14:12:30 +0100

    Heat Class Refactoring

--- TemperatureHub/Helpers/HeatHelper.cs
+++ TemperatureHub/Helpers/HeatHelper.cs
@@ -323,29 +323,56 @@ namespace TemperatureHub.Helpers
 
         };
 
-        public static double GetHeatIndexFahrenheit(double temp, double Humidity)
+        /*
+//boolean isFahrenheit: True == Fahrenheit; False == Celcius
+float DHT::computeHeatIndex(float temperature, float percentHumidity, bool isFahrenheit) {
+  // Using both Rothfusz and Steadman's equations
+  // http://www.wpc.ncep.noaa.gov/html/heatindex_equation.shtml
+  float hi;
+
+  if (!isFahrenheit)
+    temperature = convertCtoF(temperature);
+
+  hi = 0.5 * (temperature + 61.0 + ((temperature - 68.0) * 1.2) + (percentHumidity * 0.094));
+
+  if (hi > 79) {
+    hi = -42.379 +
+             2.04901523 * temperature +
+            10.14333127 * percentHumidity +
+            -0.22475541 * temperature*percentHumidity +
+            -0.00683783 * pow(temperature, 2) +
+            -0.05481717 * pow(percentHumidity, 2) +
+             0.00122874 * pow(temperature, 2) * percentHumidity +
+             0.00085282 * temperature*pow(percentHumidity, 2) +
+            -0.00000199 * pow(temperature, 2) * pow(percentHumidity, 2);
+
+    if((percentHumidity < 13) && (temperature >= 80.0) && (temperature <= 112.0))
+      hi -= ((13.0 - percentHumidity) * 0.25) * sqrt((17.0 - abs(temperature - 95.0)) * 0.05882);
+
+    else if((percentHumidity > 85.0) && (temperature >= 80.0) && (temperature <= 87.0))
+      hi += ((percentHumidity - 85.0) * 0.1) * ((87.0 - temperature) * 0.2);
+  }
+
+  return isFahrenheit ? hi : convertFtoC(hi);
+}          
+             
+             */
+
+        private static double GetHeatIndexFahrenheit(double temp, double humidity)
         {
             int TempIndex = Convert.ToInt32(Math.Round(temp)) + 57;
-            int HumidityIndex = (Convert.ToInt32(Humidity) / 5);
+            int HumidityIndex = (Convert.ToInt32(humidity) / 5);
             if ((TempIndex < 0) || (TempIndex > Table.Length))
                 throw new Exception("Invalid temperature + humidity");
             else
                 return Table[TempIndex][HumidityIndex];
         }
 
-        public static double GetHeatIndexCelsius(double temp, double Humidity)
+        public static double GetHeatIndexCelsius(double temp, double humidity)
         {
             var tempF = HeatHelper.ConvertCelsiusToFahrenheit(temp);
-            int TempIndex = Convert.ToInt32(Math.Round(tempF)) + 57;
-            int HumidityIndex = (Convert.ToInt32(Humidity) / 5);
-            if ((TempIndex < 0) || (TempIndex > Table.Length))
-            {
-                throw new Exception("Invalid temperature + humidity");
-            }
-            else
-            {
-                return HeatHelper.ConvertFahrenheitToCelsius(Table[TempIndex][HumidityIndex]);
-            }
+            var hindexF = HeatHelper.GetHeatIndexFahrenheit(tempF, humidity);
+            return HeatHelper.ConvertFahrenheitToCelsius(hindexF);
         }
 
         private static double ConvertCelsiusToFahrenheit(double temp)
