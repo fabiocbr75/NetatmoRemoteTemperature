e36d23a4d3d70fe7aabd65fc32eda8c31a0e5a93
Fabio <fabio@hobbytek.net>
2018-12-17 10:27:22 +0100

    Start to define layout page

--- TemperatureHubUI/src/app/app.component.ts
+++ TemperatureHubUI/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 import { SelectItem } from 'primeng/api';
 
 interface City {
-  name: string,
-  code: string
+  name: string;
+  code: string;
 }
 
 @Component({
@@ -15,13 +15,16 @@ export class AppComponent {
   title = 'TemperatureHubUI';
   cities1: SelectItem[];
   selectedCities1: City[];
+  from: Date;
+  to: Date;
+  data: any;
   constructor() {
     this.cities1 = [
-      {label:'New York', value:{id:1, name: 'New York', code: 'NY'}},
-      {label:'Rome', value:{id:2, name: 'Rome', code: 'RM'}},
-      {label:'London', value:{id:3, name: 'London', code: 'LDN'}},
-      {label:'Istanbul', value:{id:4, name: 'Istanbul', code: 'IST'}},
-      {label:'Paris', value:{id:5, name: 'Paris', code: 'PRS'}}
+      {label: 'New York', value: {id: 1, name: 'New York', code: 'NY'}},
+      {label: 'Rome', value: {id: 2, name: 'Rome', code: 'RM'}},
+      {label: 'London', value: {id: 3, name: 'London', code: 'LDN'}},
+      {label: 'Istanbul', value: {id: 4, name: 'Istanbul', code: 'IST'}},
+      {label: 'Paris', value: {id: 5, name: 'Paris', code: 'PRS'}}
     ];
   }
 }
