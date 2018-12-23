7b4f4fb8eb88ee43d1ba204dd39b6b099a04668f
Fabio <fabio@hobbytek.net>
2018-12-23 12:33:22 +0100

    Added GetToken

---
+++ TemperatureHub/NetatmoData/NetatmoDataHandler.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace TemperatureHub.NetatmoData
+{
+    public class NetatmoDataHandler
+    {
+        public async Task<NetatmoToken> GetToken(string clientId, string clientSecret, string userName, string password)
+        {
+            var dict = new Dictionary<string, string>();
+            dict.Add("grant_type", "password");
+            dict.Add("client_id", clientId);
+            dict.Add("client_secret", clientSecret);
+            dict.Add("username", userName);
+            dict.Add("password", password);
+            dict.Add("scope", "read_thermostat write_thermostat");
+            var client = new HttpClient();
+            var req = new HttpRequestMessage(HttpMethod.Post, "https://api.netatmo.com/oauth2/token") { Content = new FormUrlEncodedContent(dict) };
+            var res = await client.SendAsync(req);
+            var tokenJson = res.Content.ReadAsStringAsync().Result;
+            return JsonConvert.DeserializeObject<NetatmoToken>(tokenJson);
+        }
+    }
+}
