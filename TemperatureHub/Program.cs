7a0f1c128f16a8f3ce8d6c72026022cb0b78d88b
Fabio <fabio@hobbytek.net>
2018-12-24 16:12:10 +0100

    Readded Program.cs

---
+++ TemperatureHub/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TemperatureHub
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => {
+                    //options.Listen(IPAddress.Loopback, 5000); //HTTP port
+                    options.Listen(IPAddress.Any, 5000); //HTTP port
+                })
+                .SuppressStatusMessages(true)
+                .UseStartup<Startup>();
+    }
+}
