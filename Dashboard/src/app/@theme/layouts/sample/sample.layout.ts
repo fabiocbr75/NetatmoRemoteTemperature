a7dbe5e43d6997f932fdf3012a839276eecd69e5
Fabio <fabio@hobbytek.net>
2019-01-30 11:47:40 +0100

    Removed right panel

--- Dashboard/src/app/@theme/layouts/sample/sample.layout.ts
+++ Dashboard/src/app/@theme/layouts/sample/sample.layout.ts
@@ -44,13 +44,6 @@ import { StateService } from '../../../@core/utils';
         <ngx-footer></ngx-footer>
       </nb-layout-footer>
 
-      <nb-sidebar class="settings-sidebar"
-                   tag="settings-sidebar"
-                   state="collapsed"
-                   fixed
-                   [end]="sidebar.id !== 'end'">
-        <ngx-theme-settings></ngx-theme-settings>
-      </nb-sidebar>
     </nb-layout>
   `,
 })
