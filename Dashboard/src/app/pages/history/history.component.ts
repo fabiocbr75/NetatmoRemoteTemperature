b2efb7a1081899704b14d1e0ee1cfc4862c31229
Fabio <fabio@hobbytek.net>
2019-02-04 23:41:52 +0100

    History Filter

--- Dashboard/src/app/pages/history/history.component.ts
+++ Dashboard/src/app/pages/history/history.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { SensorsData, Sensor } from '../../@core/data/sensors';
 import { TemperatureHumidityData, SensorDataEx } from '../../@core/data/temperature-humidity';
+import { NbRangepickerComponent } from '@nebular/theme/components/datepicker/datepicker.component';
+import { NbCalendarRange } from '@nebular/theme';
+
 
 
 @Component({
@@ -9,11 +12,13 @@ import { TemperatureHumidityData, SensorDataEx } from '../../@core/data/temperat
   styleUrls: ['./history.component.scss'],  
   templateUrl:  './history.component.html',
 })
-export class HistoryComponent  implements OnInit {
+export class HistoryComponent  implements OnInit, OnDestroy {
   private mac: string;
   private from: string;
   private to: string;
   sensorDataEx: SensorDataEx[];
+
+  @ViewChild('rangepicker') rangepicker: NbRangepickerComponent<any>;
 
   constructor(private route: ActivatedRoute, private temperatureHumidityService: TemperatureHumidityData) { 
     var now = new Date;
@@ -21,8 +26,32 @@ export class HistoryComponent  implements OnInit {
     var startDay = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 0, 0, 0, 0));
     this.from = startDay.toISOString();
   }
+  ngOnDestroy() {
+    this.rangepicker.rangeChange.unsubscribe();
+  }
+
 
   ngOnInit() {
+    
+    this.rangepicker.rangeChange.subscribe((val: {start: Date, end: Date}) => {
+
+      var dateFrom = (val.start) ? val.start : null;
+      if (dateFrom != null) {
+        var d = new Date(Date.UTC(dateFrom.getFullYear(), dateFrom.getMonth(), dateFrom.getDate(),0,0,0))
+        this.from = d.toISOString();
+
+        var dateTo = (val.end) ? val.end : null;
+        var tmp: Date;
+        if (dateTo != null) {
+          tmp = new Date(Date.UTC(dateTo.getFullYear(), dateTo.getMonth(), dateTo.getDate(), 23, 59, 59, 999));
+          this.to = tmp.toISOString();
+          this.temperatureHumidityService.getSensorDataEx(this.mac, this.from, this.to).subscribe((data) => {
+            this.sensorDataEx = data;
+          });
+        }
+      }
+    });
+    
     this.route.paramMap.subscribe(params => {
       this.mac = params.get("mac")
       this.temperatureHumidityService.getSensorDataEx(this.mac, this.from, this.to).subscribe((data) => {
