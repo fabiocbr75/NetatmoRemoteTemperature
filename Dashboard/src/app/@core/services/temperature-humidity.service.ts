eaa437fdf859a29467779a5b0286b0cc0938751a
fabiocbr75 <fabio@hobbytek.net>
2020-09-18 15:24:11 +0200

    Change Bride IP

--- Dashboard/src/app/@core/services/temperature-humidity.service.ts
+++ Dashboard/src/app/@core/services/temperature-humidity.service.ts
@@ -7,8 +7,8 @@ import { HttpClient, HttpHeaders, HttpErrorResponse, HttpParams } from '@angular
 @Injectable()
 export class TemperatureHumidityService extends TemperatureHumidityData {
     
-  // private endpoint: string = 'http://192.168.2.40:5000/api/';
-  private endpoint: string = 'http://localhost:5000/api/';
+  private endpoint: string = 'http://192.168.2.40:5000/api/';
+  // private endpoint: string = 'http://localhost:5000/api/';
   private httpOptions: any = {
     headers: new HttpHeaders({
         'Content-Type':  'application/json'
